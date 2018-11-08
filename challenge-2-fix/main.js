b1f36bb29d0031b8b89f9e199bb4dc85dc818dc9
Lauren Reilly <LaurenReillyNet@gmail.com>
2018-11-08 15:43:20 -0500

    something happened with the challenge 2 folder so I recreated it

---
+++ challenge-2-fix/main.js
@@ -0,0 +1,53 @@
+// Initialize the FirebaseUI Widget using Firebase.
+var ui = new firebaseui.auth.AuthUI(firebase.auth());
+
+
+//Add the email provider ID to the list of FirebaseUI signInOptions.
+ui.start('#firebaseui-auth-container', {
+    signInOptions: [
+      firebase.auth.EmailAuthProvider.PROVIDER_ID
+    ],
+    // Other config options...
+  });
+
+
+
+var uiConfig = {
+    callbacks: {
+      signInSuccessWithAuthResult: function(authResult, redirectUrl) {
+        // User successfully signed in.
+        // Return type determines whether we continue the redirect automatically
+        // or whether we leave that to developer to handle.
+        return true;
+      },
+      uiShown: function() {
+        // The widget is rendered.
+        // Hide the loader.
+        document.getElementById('loader').style.display = 'none';
+      }
+    },
+    // Will use popup for IDP Providers sign-in flow instead of the default, redirect.
+    signInFlow: 'popup',
+    signInSuccessUrl: 'http://localhost:3000/challenge-2/dashboard.html',
+    signInOptions: [
+      // Leave the lines as is for the providers you want to offer your users.
+      firebase.auth.EmailAuthProvider.PROVIDER_ID
+    ]
+  };
+
+//get reference to sign out button that we've included on every page
+document.getElementById('signOut').addEventListener('click', function() {
+    //  To sign out a user, call signOut:
+    firebase.auth().signOut().then(function() {
+        alert("you signed out");
+        //to redirect to another page
+        window.location = "http://localhost:3000/challenge-2/index.html";
+    }).catch(function(error) {
+    // An error happened.
+    });
+  });
+
+
+
+  // The start method will wait until the DOM is loaded.
+ui.start('#firebaseui-auth-container', uiConfig);
