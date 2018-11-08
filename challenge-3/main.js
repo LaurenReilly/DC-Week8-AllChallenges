ae365821ae604be3374b8c5f8cc6e3614d834cee
Lauren Reilly <LaurenReillyNet@gmail.com>
2018-11-08 14:30:15 -0500

    got the submit button to add the text to the database with .push

--- challenge-3/main.js
+++ challenge-3/main.js
@@ -1,9 +1,29 @@
 var database = firebase.database();
 
+var commentList = document.getElementById("commentList");
+var posts = firebase.database().ref('posts/');
 var form = document.getElementById("commentForm");
 
+//event listener on submit that will run writeNewPost
 form.addEventListener("submit", function(e) {
     e.preventDefault();
     var comment = document.getElementById("comment").value;
-    console.log(comment);
+    writeNewPost(comment);
 });
+
+//this adds the entry to the database on submit!!!!!!!!!!
+function writeNewPost(comment) {
+    // A post entry.
+    var postData = {
+      commentBody: comment
+    };
+  
+    // Get a key for a new Post.
+    var newPostKey = firebase.database().ref().child('posts').push().key;
+  
+    // Write the post into the posts section of the database
+    var updates = {};
+    updates['posts/' + newPostKey] = postData;
+  
+    return firebase.database().ref().update(updates);
+  }
